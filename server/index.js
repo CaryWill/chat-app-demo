2d86ac26bcff2fd8ea4c53817d42ceb9afa2e9e4
cary <CaryWill.K@gmail.com>
2023-01-15 20:59:34 +0800

    fix: update cors

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 4000;
 
 //New imports
-const http = require('http').Server(app);
-const cors = require('cors');
+const http = require("http").Server(app);
+const cors = require("cors");
 
 app.use(cors());
 
-app.get('/api', (req, res) => {
+app.get("/api", (req, res) => {
   res.json({
-    message: 'Hello world',
+    message: "Hello world",
   });
 });
 
@@ -18,16 +18,8 @@ http.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
-const socketIO = require('socket.io')(http, {
-    cors: {
-        origin: "http://localhost:3000"
-    }
-});
-
-//Add this before the app.get() block
-socketIO.on('connection', (socket) => {
-    console.log(`⚡: ${socket.id} user just connected!`);
-    socket.on('disconnect', () => {
-      console.log('🔥: A user disconnected');
-    });
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
 });
