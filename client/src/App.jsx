0c1b3e9b407be7fe358e981a6199e902f218e192
cary <CaryWill.K@gmail.com>
2023-01-15 21:30:52 +0800

    fix: use ws npm package as nodejs lib

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
-import socketIO from 'socket.io-client';
-const socket = socketIO.connect('http://localhost:4000');
+import React, { useEffect } from "react";
 
 export default function App() {
+  useEffect(() => {
+    // 打开一个WebSocket:
+    var ws = new WebSocket("ws://localhost:8888/im");
+    // 响应onmessage事件:
+    ws.onmessage = function (msg) {
+      console.log(msg);
+    };
+    // 给服务器发送一个字符串:
+    setTimeout(() => {
+      ws.send("Hello!");
+    }, 1000);
+  }, []);
   return (
     <div>
       <p>Hello World!</p>
--- server/index.js
+++ server/index.js
@@ -2,13 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 
-//New imports
 const http = require("http").Server(app);
 const cors = require("cors");
 
 app.use(cors());
 
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
   res.json({
     message: "Hello world",
   });
@@ -18,8 +17,21 @@ http.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
-const socketIO = require("socket.io")(http, {
-  cors: {
-    origin: "http://localhost:3000",
-  },
+// FIXME: 刷新页面就直接变成新的用户了
+const WebSocket = require("ws");
+const WebSocketServer = WebSocket.Server;
+const wss = new WebSocketServer({
+  port: 8888,
+});
+
+wss.on("connection", function (ws) {
+  console.log(`[SERVER] connection()`);
+  ws.on("message", function (message) {
+    console.log(`[SERVER] Received: ${message}`);
+    ws.send(`ECHO: ${message}`, (err) => {
+      if (err) {
+        console.log(`[SERVER] error: ${err}`);
+      }
+    });
+  });
 });
