5e626b3f71e1113c7fd5adbffcbef90b13932509
cary <CaryWill.K@gmail.com>
2023-01-19 13:52:07 +0800

    fix: add close func

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,10 +21,16 @@ class Connection {
     };
 
     this.ws.onclose = (event) => {
+      const { code, reason, wasClean } = event;
+      console.error(
+        `Connection closed, code: ${code}, reason: ${reason}, wasClean: ${wasClean}`
+      );
       if (this.retryCount >= this.maxRetryCount) {
         console.error("unable to make a connection!");
       } else {
         this.retryCount += 1;
+        // 这里不需要在建立新的 ws 连接前手动 `this.ws.close()`
+        // 因为已经是 onclose 事件了，说明 ws 已经断掉了
         setTimeout(() => {
           this.open();
           console.log(`reconecting x ${this.retryCount}`);
@@ -35,6 +41,10 @@ class Connection {
     this.ws.onerror = () => {
       console.error("ws connected error!");
     };
+  }
+
+  close() {
+    this.ws.close();
   }
 }
 export default function App() {
